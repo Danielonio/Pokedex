182584048e0e3676973e02228f523d260bcd5011
Danielonio <43379049+Danielonio@users.noreply.github.com>
2019-04-02 11:33:54 +0200

    Funcion thanos que busca lo que sea

--- Project/index.js
+++ Project/index.js
@@ -1,15 +1,46 @@
 var mongo = require('mongodb')
 var MongoClient = require('mongodb').MongoClient;
 
-var url = "mongodb://localhost:27017/newdb";
+var url = "mongodb://localhost:27017/pokeDB";
 
 MongoClient.connect(url,
-    function(err, db) {
+    function(err, db) 
+    {
     if (err) throw err;
     console.log("Database connected!");
-    var dbo = db.db("newdb");
-    var results = dbo.collection("students").find({});
-    results.forEach(row => {
+    var dbo = db.db("pokeDB");
+    var results = filtroThanos(dbo,1,"iron",1)
+    results.forEach(row => 
+        {
         console.log(row);
-    });
-});
+        });
+    }
+);
+
+function filtroThanos(dbo,gen,type,leg)
+{ 
+    if(gen==-1 && type==-1 && leg ==-1)
+        {
+            var results = dbo.collection("pokeCollection").find();
+        }
+    else
+        {
+            var filter1 = {$and: []};
+            if(gen!=-1)
+                {
+                    filter1.$and.push({generation: gen});
+                }
+            if(type!=-1)
+                {
+                    filter1.$and.push({$or: [{type1:type},{type2:type}]});
+                }
+            if(leg!=-1)
+                {
+                    filter1.$and.push({is_legendary:1});
+                }
+
+                var results = dbo.collection("pokeCollection").find(filter1);
+        }
+
+    return results;
+}
