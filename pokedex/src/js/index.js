b066964ea9625dd23e401c9da7e05c5888246ede
Who1ne <43378932+Who1ne@users.noreply.github.com>
2019-04-17 03:53:49 +0200

    Comunicación básica JS-HTML

---
+++ pokedex/src/js/index.js
@@ -0,0 +1,5 @@
+var nombre = "Nombre de Pokemon";
+var detalles = "Descripción detallada de las habilidades del Pokemon y toda la pesca"
+
+document.getElementById('nombre').innerHTML = nombre;
+document.getElementById('detalles').innerHTML = detalles;
