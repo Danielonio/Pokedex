a744fb82fdcec2dde76eb69da2cbb8288d2d2794
Who1ne <43378932+Who1ne@users.noreply.github.com>
2019-04-19 17:30:46 +0200

    Actualizaciones de interfaz de Carlos
    (Me las había cargado)

--- Project/js/index.js
+++ Project/js/index.js
@@ -3,23 +3,28 @@ var detalles = "Descripción detallada de las habilidades del Pokemon y toda la 
 var nPokemon = 5;
 var pokemon = "e";
 
+
 document.getElementById('nombre').innerHTML = nombre;
 document.getElementById('detalles').innerHTML = detalles;
 
-var pokemones = ["pokemon1", "pokemon2", "pokemon3", "otro más"];
-var numeros = [1, 40, 321, 390];
-var generaciones = [1, 2, 6, 6];
+var pokemones = ["pokemon1", "pokemon2", "pokemon3", "otro más","pokemon1", "pokemon2", "pokemon3", "otro más","pokemon1", "pokemon2", "pokemon3", "otro más"];
+var numeros = [1, 40, 321, 390,1, 40, 321, 390,1, 40, 321, 390];
+var generaciones = [1, 2, 6, 6,1, 2, 6, 6,1, 2, 6, 6];
 
 pokemones.forEach((value, i) => {
     var td1 = document.createElement('td');
     var td2 = document.createElement('td');
     var tr = document.createElement('tr');
     var th = document.createElement('th');
+	var tbody = document.createElement('tbody');
+	tbody.appendChild(th);
     th.appendChild(document.createTextNode(numeros[i]));
     td1.appendChild(document.createTextNode(value));
     td2.appendChild(document.createTextNode(generaciones[i]));
     tr.appendChild(th);
     tr.appendChild(td1);
     tr.appendChild(td2);
-    document.getElementById('pokemonTable').appendChild(tr);
+	tbody.appendChild(tr)
+
+    document.getElementById('pokemonTable').appendChild(tbody);
 });
